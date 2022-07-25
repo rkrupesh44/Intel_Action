042c2bbdd1b26b0ab34a381add8463a54df444da
rkrupesh44 <rupeshcuj@gmail.com>
2022-07-25 19:20:15 +0530

    Update dep.sh

--- .github/workflows/dep.sh
+++ .github/workflows/dep.sh
@@ -1,3 +1,3 @@
-curl -X POST -H "Accept: application/vnd.github+json" -u rupeshcuj@gmail.com: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 \
+curl -X POST -H "Accept: application/vnd.github+json" -u rupeshcuj@gmail.com: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 \
 https://api.github.com/repos/rkrupesh44/Intel_Action/actions/workflows/manual.yml/dispatches \
 -d '{"ref":"main"}'
