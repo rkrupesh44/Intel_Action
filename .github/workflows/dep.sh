bb45eb2edbaadb3ed6b066a12fe95eb5814c18a6
Rupesh Kumar <rupeshcuj@gmail.com>
2022-07-25 13:38:32 +0000

    dev changes

--- .github/workflows/dep.sh
+++ .github/workflows/dep.sh
@@ -1,3 +1,3 @@
 curl -X POST -H "Accept: application/vnd.github+json" -u rupeshcuj@gmail.com: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 \
 https://api.github.com/repos/rkrupesh44/Intel_Action/actions/workflows/manual.yml/dispatches \
--d '{"ref":"main"}'
+-d '{"ref":"dev"}'
