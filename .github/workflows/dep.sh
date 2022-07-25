4453ac2b4462dc9ddc5b156cfe9eb19020ac0c7e
rkrupesh44 <88672591+rkrupesh44@users.noreply.github.com>
2022-07-25 18:13:59 +0530

    Update dep.sh

--- .github/workflows/dep.sh
+++ .github/workflows/dep.sh
@@ -1,6 +1,6 @@
 curl \
 -X POST \
 -H "Accept: application/vnd.github+json" \ 
--H "Authorization: token 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" \
+-H "Authorization: token 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" \
 https://api.github.com/repos/rkrupesh44/Intel_Action/actions/workflows/manual.yml/dispatches \
 -d '{"ref":"main"}
