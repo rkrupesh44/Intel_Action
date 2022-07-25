0b49efa0589446f3e56e7a5fd177df38c1ba9c92
Rupesh Kumar <rupeshcuj@gmail.com>
2022-07-25 13:07:56 +0000

    deploy file mofification

--- .github/workflows/dep.sh
+++ .github/workflows/dep.sh
@@ -1,6 +1,3 @@
-curl \
--X POST \
--H "Accept: application/vnd.github+json" \ 
--H "Authorization: token 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" \
+curl -X POST -H "Accept: application/vnd.github+json" -u rkrupesh44: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 \
 https://api.github.com/repos/rkrupesh44/Intel_Action/actions/workflows/manual.yml/dispatches \
--d '{"ref":"main"}
+-d '{"ref":"main"}'
